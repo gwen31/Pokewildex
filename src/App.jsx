8e0fd7e3cace27e30ea1092655a533fce9b32e03
Antho <anthony.deplanque3184@gmail.com>
2021-04-14 12:26:45 +0200

    added firt list-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { useState } from 'react';
+import PokedexList from './Components/PokedexList';
+import PokedexDetails from './Components/PokedexDetails';
 import Navbar from './Container/Navbar';
 
 function App() {
+  const [selectedPokemonId, setSelectedPokemonId] = useState(null);
   return (
     <div className="App">
       <Navbar />
+      <PokedexList setSelectedPokemonId={setSelectedPokemonId} />
+      {selectedPokemonId && (
+        <PokedexDetails key={selectedPokemonId} pokemonId={selectedPokemonId} />
+      )}
     </div>
   );
 }
