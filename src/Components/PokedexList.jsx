be1aa93d9a57c2126b5567885e9c25cd5bed86f7
Antho <anthony.deplanque3184@gmail.com>
2021-04-14 16:39:38 +0200

    update PokedexList.jsx

--- src/Components/PokedexList.jsx
+++ src/Components/PokedexList.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import PokedexDetails from './PokedexDetails';
 
 const apiDefault = 'https://pokeapi.co/api/v2/pokemon';
-function PokedexList({ setSelectedPokemonId }) {
+function PokedexList() {
   const [pokemons, setPokemons] = useState([]);
   const [currPage, setCurrPage] = useState(apiDefault);
   const [nextPage, setNextPage] = useState([]);
@@ -27,17 +28,7 @@ function PokedexList({ setSelectedPokemonId }) {
       <>
         <ul>
           {pokemons.map((pokemon) => (
-            <li>
-              <a
-                href={pokemon.name}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSelectedPokemonId(pokemon.name);
-                }}
-              >
-                {pokemon.name}
-              </a>
-            </li>
+            <PokedexDetails key={pokemon.name} pokemonId={pokemon.name} />
           ))}
         </ul>
         {prevPage && (
