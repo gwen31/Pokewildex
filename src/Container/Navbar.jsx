713c8afd2043157e0ca3a30b8eef9292efa1fb58
Thomas Jondeau <thomas-jondeau@hotmail.fr>
2021-04-14 14:13:54 +0200

    navbar inprogress

--- src/Container/Navbar.jsx
+++ src/Container/Navbar.jsx
@@ -6,6 +6,45 @@ import StaffList from '../Components/StaffList';
 const Navbar = () => (
   <div>
     <h1>PokeWilDex</h1>
+    <nav className="sidebar">
+      <header>My App</header>
+      <ul>
+        <li>
+          <a href="toto">
+            <img
+              className="sidebar-icon"
+              src="images/icons/pokedex.png"
+              alt=""
+            />
+            Pokedex
+          </a>
+        </li>
+        <li>
+          <a href="toto">
+            <img src="images/icons/random.png" alt="" />
+            Team Generator
+          </a>
+        </li>
+        <li>
+          <a href="toto">
+            <img src="images/icons/balance.png" alt="" />
+            Comparator
+          </a>
+        </li>
+        <li>
+          <a href="toto">
+            <img src="images/icons/versus.png" alt="" />
+            Combat
+          </a>
+        </li>
+        <li>
+          <a href="toto">
+            <img src="images/icons/cap.png" alt="" />
+            Staff
+          </a>
+        </li>
+      </ul>
+    </nav>
     <Comparator />
     <Randomteam />
     <TypesList />
