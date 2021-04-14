1598a56dfc7abc88490ff831a340b08b37603e6e
Thomas Jondeau <thomas-jondeau@hotmail.fr>
2021-04-14 13:43:03 +0200

    fixed navbar

---
+++ src/Components/RandomTeam.jsx
@@ -0,0 +1,5 @@
+function RandomTeam() {
+  return null;
+}
+
+export default RandomTeam;
--- src/Container/Navbar.jsx
+++ src/Container/Navbar.jsx
@@ -1,14 +1,12 @@
 import Comparator from '../Components/Comparator';
-import PokedexList from '../Components/PokedexList';
 import Randomteam from '../Components/RandomTeam';
 import TypesList from '../Components/TypesList';
 import StaffList from '../Components/StaffList';
 
 const Navbar = () => (
   <div>
-    <h1>pokewilddex</h1>
+    <h1>PokeWilDex</h1>
     <Comparator />
-    <PokedexList />
     <Randomteam />
     <TypesList />
     <StaffList />
