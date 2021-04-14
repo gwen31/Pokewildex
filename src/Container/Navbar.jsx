ff0f973e2ddd248cfe7e3dbca2777bc6cfd6b707
Thomas Jondeau <thomas-jondeau@hotmail.fr>
2021-04-14 16:04:56 +0200

    up css

--- src/Container/Navbar.jsx
+++ src/Container/Navbar.jsx
@@ -2,54 +2,57 @@ import Comparator from '../Components/Comparator';
 import Randomteam from '../Components/RandomTeam';
 import TypesList from '../Components/TypesList';
 import StaffList from '../Components/StaffList';
+import './Navbar.css';
+import imgPokedex from './images/icons/pokedex.png';
+import imgRandom from './images/icons/random.png';
+import imgVersus from './images/icons/versus.png';
+import imgCap from './images/icons/cap.png';
+import imgBalance from './images/icons/balance.png';
 
-const Navbar = () => (
-  <div>
-    <h1>PokeWilDex</h1>
-    <nav className="sidebar">
-      <header>My App</header>
-      <ul>
-        <li>
-          <a href="toto">
-            <img
-              className="sidebar-icon"
-              src="images/icons/pokedex.png"
-              alt=""
-            />
-            Pokedex
-          </a>
-        </li>
-        <li>
-          <a href="toto">
-            <img src="images/icons/random.png" alt="" />
-            Team Generator
-          </a>
-        </li>
-        <li>
-          <a href="toto">
-            <img src="images/icons/balance.png" alt="" />
-            Comparator
-          </a>
-        </li>
-        <li>
-          <a href="toto">
-            <img src="images/icons/versus.png" alt="" />
-            Combat
-          </a>
-        </li>
-        <li>
-          <a href="toto">
-            <img src="images/icons/cap.png" alt="" />
-            Staff
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <Comparator />
-    <Randomteam />
-    <TypesList />
-    <StaffList />
-  </div>
-);
+function Navbar() {
+  return (
+    <div>
+      <div className="sidebar">
+        <header>PokeWilDex</header>
+        <ul>
+          <li>
+            <a href="toto">
+              <img className="sidebar-icon" src={imgPokedex} alt="" />
+              Pokedex
+            </a>
+          </li>
+          <li>
+            <a href="toto">
+              <img className="sidebar-icon" src={imgRandom} alt="" />
+              Team Generator
+            </a>
+          </li>
+          <li>
+            <a href="toto">
+              <img className="sidebar-icon" src={imgBalance} alt="" />
+              Comparator
+            </a>
+          </li>
+          <li>
+            <a href="toto">
+              <img className="sidebar-icon" src={imgVersus} alt="" />
+              Combat
+            </a>
+          </li>
+          <li>
+            <a href="toto">
+              <img className="sidebar-icon" src={imgCap} alt="" />
+              Staff
+            </a>
+          </li>
+        </ul>
+      </div>
+      <Comparator />
+      <Randomteam />
+      <TypesList />
+      <StaffList />
+    </div>
+  );
+}
 
 export default Navbar;
