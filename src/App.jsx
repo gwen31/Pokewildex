7b183538ce57377e0003ecae9ebb98ff6ed68d68
Thomas Jondeau <thomas-jondeau@hotmail.fr>
2021-04-16 10:36:11 +0200

    create rooter and rectif css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import PokedexList from './Components/PokedexList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Container/Navbar';
+import PokedexList from './Components/PokedexList';
+import Comparator from './Components/Comparator';
+import RandomTeam from './Components/RandomTeam';
+import StaffList from './Components/StaffList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar">
+      <Router>
         <Navbar />
-      </div>
-      <div className="main">
-        <PokedexList />
-      </div>
+        <div className="main">
+          <Switch>
+            <Route path="/PokedexList">
+              <PokedexList />
+            </Route>
+            <Route path="/RandomTeam">
+              <RandomTeam />
+            </Route>
+            <Route path="/Comparator">
+              <Comparator />
+            </Route>
+            <Route path="/StaffList">
+              <StaffList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
