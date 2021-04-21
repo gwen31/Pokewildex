1f02418752afdc284d27da11ec39ae7e8ca8856e
Thomas Jondeau <thomas-jondeau@hotmail.fr>
2021-04-21 12:25:07 +0200

    changed function's name

--- src/Components/RandomTeam.jsx
+++ src/Components/RandomTeam.jsx
@@ -2,17 +2,17 @@ import PokedexDetails from './PokedexDetails';
 
 const min = 1;
 const max = 898;
-const randomize = () => Math.floor(Math.random() * max) + min;
+const getRandomPokemonId = () => Math.floor(Math.random() * max) + min;
 
 function RandomTeam() {
   return (
     <div className="pokedex-cards">
-      <PokedexDetails pokemonId={randomize()} />
-      <PokedexDetails pokemonId={randomize()} />
-      <PokedexDetails pokemonId={randomize()} />
-      <PokedexDetails pokemonId={randomize()} />
-      <PokedexDetails pokemonId={randomize()} />
-      <PokedexDetails pokemonId={randomize()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
+      <PokedexDetails pokemonId={getRandomPokemonId()} />
     </div>
   );
 }
