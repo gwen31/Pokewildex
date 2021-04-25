3c29f35a0b9932bec53aa246ffed9250830eea13
Antho <anthony.deplanque3184@gmail.com>
2021-04-25 23:22:58 +0200

    added a ScrollButton

--- src/Container/ScrollButton.jsx
+++ src/Container/ScrollButton.jsx
@@ -1,12 +1,25 @@
 import PokeballArrow from './images/icons/poke-ball-arrow.png';
 import './ScrollButton.css';
 
-export default function ScrollButton() {
+const mybutton = document.getElementsByClassName('go-up-btn');
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > 400
+    || document.documentElement.scrollTop > 400
+  ) {
+    mybutton[0].style.display = 'block';
+  } else {
+    mybutton[0].style.display = 'none';
+  }
+};
+
+function ScrollButton() {
   return (
-    <div className="go-up-btn">
+    <div className="go-up-btn" id="scroll-btn">
       <a href="#top">
         <img src={PokeballArrow} className="go-up-icon" alt="scroll up icon" />
       </a>
     </div>
   );
 }
+export default ScrollButton;
