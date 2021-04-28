c409ff6d1aa7210f74e029edba0f00c0acc40b78
Antho <anthony.deplanque3184@gmail.com>
2021-04-28 11:53:45 +0200

    colorized card based on pokemon type

--- src/Components/PokedexDetails.jsx
+++ src/Components/PokedexDetails.jsx
@@ -2,53 +2,82 @@ import LoadingElement from './LoadingElement';
 import LoadingScreen from './LoadingScreen';
 import './PokedexDetails.css';
 import PokemonFetchApi from './PokemonFetchApi';
+import colorTypes from '../constants/colorTypes';
 
 function PokedexDetails({ pokemonId }) {
   const pokemonInfos = PokemonFetchApi(pokemonId);
   const {
     id, sprites, types, abilities, species,
   } = pokemonInfos;
+
+  const typesForCard = [];
+  const colorsForCard = [];
+  if (types) {
+    typesForCard.push(types[0].type.name);
+    colorsForCard.push(colorTypes[typesForCard[0]]);
+    if (types.length > 1) {
+      typesForCard.push(types[1].type.name);
+      colorsForCard.push(colorTypes[typesForCard[1]]);
+    }
+  }
   return (
     <>
       <div className="pokedex-card">
-        <div className="pokedex-card-additional">
-          <div className="more-infos">
-            {sprites ? (
-              <img
-                className="pokedex-card-image"
-                src={sprites.front_default}
-                alt={id}
-              />
-            ) : (
-              <LoadingElement />
-            )}
-            <div className="pokedex-card-description pokedex-card-right">
-              <section className="responsive-description">
-                <p className="pokedex-card-description">Id : {id}</p>
-                {types ? (
-                  <ul className="pokedex-card-description">
-                    type:
-                    {types.map((e) => (
-                      <li>{e.type.name}</li>
-                    ))}
-                  </ul>
-                ) : (
-                  <LoadingElement />
-                )}
-                {abilities ? (
-                  <ul className="pokedex-card-description">
-                    Abilities :
-                    {abilities.map((e) => (
-                      <li>{e.ability.name}</li>
-                    ))}
-                  </ul>
-                ) : (
-                  <LoadingElement />
-                )}
-              </section>
+        {types ? (
+          <div
+            className="pokedex-card-additional"
+            style={
+              colorsForCard[1]
+                ? {
+                  background: `linear-gradient(${colorsForCard[0]}, ${colorsForCard[1]})`,
+                  'border-radius': '10px',
+                }
+                : {
+                  background: colorsForCard[0],
+                  'border-radius': '10px',
+                }
+            }
+          >
+            <div className="more-infos">
+              {sprites ? (
+                <img
+                  className="pokedex-card-image"
+                  src={sprites.front_default}
+                  alt={id}
+                />
+              ) : (
+                <LoadingElement />
+              )}
+              <div className="pokedex-card-description pokedex-card-right">
+                <section className="responsive-description">
+                  <p className="pokedex-card-description">Id : {id}</p>
+                  {types ? (
+                    <ul className="pokedex-card-description">
+                      type:
+                      {types.map((e) => (
+                        <li>{e.type.name}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <LoadingElement />
+                  )}
+                  {abilities ? (
+                    <ul className="pokedex-card-description">
+                      Abilities :
+                      {abilities.map((e) => (
+                        <li>{e.ability.name}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <LoadingElement />
+                  )}
+                </section>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <LoadingScreen />
+        )}
         <div className="pokedex-card-center name">
           {species ? (
             <h2 className="pokemon-police">{species.name}</h2>
@@ -60,5 +89,4 @@ function PokedexDetails({ pokemonId }) {
     </>
   );
 }
-
 export default PokedexDetails;
