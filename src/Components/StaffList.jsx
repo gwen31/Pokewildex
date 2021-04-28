0e6cf467dddc27194e693ce180fb497b5b42261a
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-04-28 10:58:56 +0200

    added template card for stafflist and created the css file to go with it

--- src/Components/StaffList.jsx
+++ src/Components/StaffList.jsx
@@ -1,5 +1,31 @@
+import './StaffList.css';
+
 function StaffList() {
-  return null;
+  return (
+    <article className="card">
+      <div className="card-body">
+        <img src="" alt="" className="card-body-img" />
+        <h1 className="card-body-title">
+          <span> </span>
+        </h1>
+        <p className="card-body-text"> </p>
+      </div>
+      <div className="card-footer">
+        <div className="card-footer-social">
+          <h3> </h3>
+          <p>Attack</p>
+        </div>
+        <div className="card-footer-social">
+          <h3> </h3>
+          <p>Special Attack</p>
+        </div>
+        <div className="card-footer-social">
+          <h3> </h3>
+          <p>Defense</p>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default StaffList;
