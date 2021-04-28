efc994dc8d3068b71c3098a40a25eb0c6599ae5d
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-04-28 17:01:46 +0200

    styled staff card and made it responsive

--- src/Components/StaffList.jsx
+++ src/Components/StaffList.jsx
@@ -1,30 +1,58 @@
-import './StaffList.css';
+import Staff from './Staff';
+
+const staffs = [
+  {
+    name: 'Anthony',
+    data: {
+      color: '#6FE457',
+      imgUrl:
+        'https://cdn.discordapp.com/attachments/829002496043122688/836549202943148079/Pokemon-antho.jpg',
+    },
+    age: '36 ans',
+    id: 1,
+  },
+  {
+    name: 'Chloé',
+    data: {
+      imgUrl: 'https://i.ibb.co/jgBvZ3M/rsz-pokechloe.jpg',
+      color: '#FE92CA',
+    },
+    age: '24 ans',
+    id: 2,
+  },
+  {
+    name: 'Gwénaëlle',
+    data: {
+      imgUrl:
+        'https://cdn.discordapp.com/attachments/834053825794539540/835195506597822554/inCollage_20210423_184807798.jpg',
+      color: '#40A5FC',
+    },
+    age: '43 ans',
+    id: 3,
+  },
+  {
+    name: 'Thomas',
+    data: {
+      imgUrl: '',
+      color: '#606060',
+    },
+    age: '29 ans',
+    id: 4,
+  },
+];
 
 function StaffList() {
   return (
-    <article className="card">
-      <div className="card-body">
-        <img src="" alt="" className="card-body-img" />
-        <h1 className="card-body-title">
-          <span> </span>
-        </h1>
-        <p className="card-body-text"> </p>
-      </div>
-      <div className="card-footer">
-        <div className="card-footer-social">
-          <h3> </h3>
-          <p>Attack</p>
-        </div>
-        <div className="card-footer-social">
-          <h3> </h3>
-          <p>Special Attack</p>
-        </div>
-        <div className="card-footer-social">
-          <h3> </h3>
-          <p>Defense</p>
-        </div>
-      </div>
-    </article>
+    <div className="wrap">
+      {staffs.map((staff) => (
+        <Staff
+          key={staff.id}
+          name={staff.name}
+          data={staff.data}
+          age={staff.age}
+        />
+      ))}
+    </div>
   );
 }
 
