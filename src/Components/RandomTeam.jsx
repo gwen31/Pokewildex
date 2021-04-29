af17efdaea725c3750906be2501cd309b4cdf7c8
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-04-29 09:52:58 +0200

    creation header on Team Generator

--- src/Components/RandomTeam.jsx
+++ src/Components/RandomTeam.jsx
@@ -1,9 +1,13 @@
 import PokedexDetails from './PokedexDetails';
 import getRandomPokemonId from '../utils/pokemons';
+import Header from '../Container/Header';
 
 function RandomTeam() {
   return (
     <div className="pokedex-cards">
+      <div className="header">
+        <Header />
+      </div>
       <PokedexDetails pokemonId={getRandomPokemonId()} />
       <PokedexDetails pokemonId={getRandomPokemonId()} />
       <PokedexDetails pokemonId={getRandomPokemonId()} />
