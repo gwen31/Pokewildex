63206380ae785432b8b1aa4e7d65b815c577a62c
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-05-04 11:09:59 +0200

    first idea of a comparator

--- src/Components/Comparator.jsx
+++ src/Components/Comparator.jsx
@@ -1,5 +1,64 @@
-function Comparator() {
-  return null;
-}
+import axios from 'axios';
+import { useState } from 'react';
+import PokedexDetails from './PokedexDetails';
+import API_POKEMON_DEFAULT from '../constants/api';
 
+function Comparator() {
+  const [searchedName1, setSearchedName1] = useState('');
+  const [searchedName2, setSearchedName2] = useState('');
+  const handleSearch = (pokemon, setPokemon) => {
+    const nameToLower = pokemon.toLowerCase();
+    setPokemon(nameToLower);
+  };
+  const fetchPokemon = (pokemonName, setPokemonName) => {
+    axios
+      .get(`${API_POKEMON_DEFAULT}${pokemonName}`)
+      .then((result) => setPokemonName([result.data]))
+      .catch((err) => {
+        if (err.response.data === 'Not Found') {
+          alert("This pokemon doesn't exist ! ");
+        }
+      });
+  };
+  const handleSubmit = (pokemon, setPokemon) => fetchPokemon(pokemon, setPokemon);
+
+  return (
+    <div className="header">
+      <input
+        className="explorer"
+        placeholder="searchbar"
+        onChange={(e) => handleSearch(e.target.value, setSearchedName1)}
+      />
+      {searchedName1 && (
+        <button
+          className="explorer"
+          type="submit"
+          onClick={() => handleSubmit(searchedName1, setSearchedName1)}
+        >
+          GO
+        </button>
+      )}{' '}
+      <input
+        className="explorer"
+        placeholder="searchbar"
+        onChange={(e) => handleSearch(e.target.value, setSearchedName2)}
+      />
+      {searchedName2 && (
+        <button
+          className="explorer"
+          type="submit"
+          onClick={() => handleSubmit(searchedName2, setSearchedName2)}
+        >
+          GO
+        </button>
+      )}
+      {searchedName1 && (
+        <PokedexDetails key={searchedName1} pokemonId={searchedName1} />
+      )}
+      {searchedName2 && (
+        <PokedexDetails key={searchedName2} pokemonId={searchedName2} />
+      )}
+    </div>
+  );
+}
 export default Comparator;
