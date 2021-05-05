507d96d0bd7dd9738fe1d22151002e297281fbe3
gwenaelle_merle <gwenaelle-merle_student2021@wilder.school>
2021-05-05 15:29:07 +0200

    css comparatot

--- src/Components/PokemonCompared.jsx
+++ src/Components/PokemonCompared.jsx
@@ -1,36 +1,46 @@
+import './PokemonCompared.css';
+
 export default function PokemonCompared({ pokemon, setPokemonSearched }) {
   const {
     sprites, types, stats, species,
   } = pokemon;
   return (
-    <div>
-      <div>
+    <div className="comparator">
+      <div className="search-bar">
         <input
           placeholder="search a fOOkin pokemon, mate !"
           onChange={(e) => setPokemonSearched(e.target.value)}
         />
-        {species && <p>{species.name}</p>}
-        {sprites && <img src={sprites.front_default} alt={species.name} />}
-        {types && (
-          <ul>
-            {types.map((e) => (
-              <li>{e.type.name}</li>
-            ))}
-          </ul>
-        )}
-        {stats && (
-          <ul>
-            {stats.map((e) => (
-              <>
-                <li>
-                  <p>
-                    {e.stat.name} : {e.base_stat}
-                  </p>
-                </li>
-              </>
-            ))}
-          </ul>
-        )}
+        <div className="comparator-card">
+          {species && <h1>{species.name}</h1>}
+          {sprites && (
+            <img
+              className="pokemon-image"
+              src={sprites.front_default}
+              alt={species.name}
+            />
+          )}
+          {types && (
+            <ul>
+              {types.map((e) => (
+                <li>{e.type.name}</li>
+              ))}
+            </ul>
+          )}
+          {stats && (
+            <ul>
+              {stats.map((e) => (
+                <>
+                  <li>
+                    <p>
+                      {e.stat.name} : {e.base_stat}
+                    </p>
+                  </li>
+                </>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
