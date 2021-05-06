4d09d1b28c0535b712c0c4929d0fe541cae6c1c4
Antho <anthony.deplanque3184@gmail.com>
2021-05-06 09:45:30 +0200

    background colors for card based by the type

--- src/Components/PokemonCompared.jsx
+++ src/Components/PokemonCompared.jsx
@@ -1,9 +1,17 @@
 import './PokemonCompared.css';
+import { useEffect, useState } from 'react';
+import setColorTypes from '../utils/setColorTypes';
 
 export default function PokemonCompared({ pokemon, setPokemonSearched }) {
   const {
     sprites, types, stats, species,
   } = pokemon;
+  const [colorsForCard, setColorsForCard] = useState([]);
+
+  useEffect(() => {
+    setColorTypes(pokemon, setColorsForCard);
+  }, [pokemon]);
+
   return (
     <div className="comparator">
       <div className="search-bar">
@@ -11,40 +19,55 @@ export default function PokemonCompared({ pokemon, setPokemonSearched }) {
           placeholder="search a pokemon to compare"
           onChange={(e) => setPokemonSearched(e.target.value)}
         />
-        <div className="comparator-card">
-          {species && <h1 className="pokemon-police">{species.name}</h1>}
-          {sprites && (
-            <img
-              className="pokemon-image"
-              src={sprites.front_default}
-              alt={species.name}
-            />
-          )}
-          <div className="comparator-text">
-            <div className="comparator-types">
-              {types && (
+        {types && (
+          <div
+            className="comparator-card"
+            style={
+              colorsForCard[1]
+                ? {
+                  background: `linear-gradient(${colorsForCard[0]}, ${colorsForCard[1]})`,
+                  'border-radius': '10px',
+                }
+                : {
+                  background: colorsForCard[0],
+                  'border-radius': '10px',
+                }
+            }
+          >
+            {species && <h1 className="pokemon-police">{species.name}</h1>}
+            {sprites && (
+              <img
+                className="pokemon-image"
+                src={sprites.front_default}
+                alt={species.name}
+              />
+            )}
+            <div className="comparator-text">
+              <div className="comparator-types">
+                {types && (
+                  <ul>
+                    {types.map((e) => (
+                      <li>{e.type.name}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              {stats && (
                 <ul>
-                  {types.map((e) => (
-                    <li>{e.type.name}</li>
+                  {stats.map((e) => (
+                    <>
+                      <li>
+                        <p>
+                          {e.stat.name} : {e.base_stat}
+                        </p>
+                      </li>
+                    </>
                   ))}
                 </ul>
               )}
             </div>
-            {stats && (
-              <ul>
-                {stats.map((e) => (
-                  <>
-                    <li>
-                      <p>
-                        {e.stat.name} : {e.base_stat}
-                      </p>
-                    </li>
-                  </>
-                ))}
-              </ul>
-            )}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
